95b1c94ece6aa4261b8a443caef7f84d9b898ab1
suhyeon0111 <soo4833@naver.com>
2025-03-26 23:28:17 +0900

    [feat]: 마이페이지 틀 생성

--- myWebPage/src/Components/MyPage/MyPage.tsx
+++ myWebPage/src/Components/MyPage/MyPage.tsx
@@ -1,10 +1,28 @@
 import { useState } from "react";
+import { LuCircleUserRound } from "react-icons/lu";
 import { useNavigate, useLocation } from "react-router-dom";
 
 export default function MyPage() {
     const location = useLocation();
     const userName = location.state?.userName || "Guest";
+    const navigate = useNavigate();
+
+    const onClickLogout = () => {
+        navigate('/');
+    }
+
     return (
-        <div>{userName}</div>
+        <div className="MyPageContainer">
+            <div>
+                <LuCircleUserRound />
+                {userName}
+            </div>
+            <div>
+                <button>다크모드</button>
+            </div>
+            <div>
+                <button onClick={onClickLogout}>로그아웃</button>
+            </div>
+        </div>
     )
 }
