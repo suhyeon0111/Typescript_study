cc24975c8c7d6e4bae5150ae94ed1d3a089b2a3b
suhyeon0111 <soo4833@naver.com>
2025-04-12 23:56:33 +0900

    [feat]: 다크모드 적용시 로고버튼 색도 함께 변경

--- myWebPage/client/src/Components/MainPage/Logo.tsx
+++ myWebPage/client/src/Components/MainPage/Logo.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
+
 export default function Logo() {
     const navigate = useNavigate();
 
     const LogoButton = styled.button`
+        height: 70px;
+        margin-top: 40px;
+        background-color: ${({ theme }) => theme.bgColor};
         color: #6DAEDB;
         position: fixed;  // 화면기준으로 위치 고정
         z-index: 999;  // 다른 요소들 위로 올라오게
@@ -18,7 +22,7 @@ export default function Logo() {
 
 
     return (
-        <LogoButton onClick={handlerClickLogo} style={{ backgroundColor: "#FFFFFF" }}>
+        <LogoButton onClick={handlerClickLogo} >
             <h1>Todo</h1>
         </LogoButton>
     )
