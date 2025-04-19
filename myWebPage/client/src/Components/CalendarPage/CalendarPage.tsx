46bdcfe447f3bc7474fdf76de4441368c56b38dd
suhyeon0111 <soo4833@naver.com>
2025-04-19 23:55:22 +0900

    feat: react-calendar모듈 사용

--- myWebPage/client/src/Components/CalendarPage/CalendarPage.tsx
+++ myWebPage/client/src/Components/CalendarPage/CalendarPage.tsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
+import Logo from '../MainPage/Logo';
+
+
+type Value = Date | null;
 
 export default function CalendarPage() {
+    const [date, setDate] = useState<Value>(new Date());
+
 
     return (
         <>
+            <Logo />
+            <Calendar
+                className="calendar"
+                onChange={(value: Value) => setDate(value)}
+                value={date}
+                selectRange={false}
+            />
         </>
     )
 }
