6c016e69db9f0570d2d64ef9dd01ae9bf4579293
suhyeon0111 <soo4833@naver.com>
2025-04-20 17:07:33 +0900

    feat: react-calendar css 중앙정렬

--- myWebPage/client/src/Components/CalendarPage/CalendarPage.tsx
+++ myWebPage/client/src/Components/CalendarPage/CalendarPage.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
+import "../../styles/Calendar.css";
 
 import Logo from '../MainPage/Logo';
 
@@ -14,12 +15,14 @@ export default function CalendarPage() {
     return (
         <>
             <Logo />
-            <Calendar
-                className="calendar"
-                onChange={(value: Value) => setDate(value)}
-                value={date}
-                selectRange={false}
-            />
+            <div className="calendarContainer">
+                <Calendar
+                    onChange={(value: Value) => setDate(value)}
+                    value={date}
+                    selectRange={false}
+
+                />
+            </div>
         </>
     )
 }
