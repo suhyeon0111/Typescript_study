d12a2dc7db5cd5cd4c5929b6f0ba40be0822ce6c
suhyeon0111 <soo4833@naver.com>
2025-04-24 23:52:11 +0900

    feat: 리스트 완료 버튼 누르면 완료로 바뀜

--- myWebPage/client/src/Components/CalendarPage/ModalTodoList.tsx
+++ myWebPage/client/src/Components/CalendarPage/ModalTodoList.tsx
@@ -4,6 +4,7 @@ import ModalTodoItem from "./ModalTodoItem";
 // 초기 틀 설정
 export interface ModalTList {
     key: number;
+    id: number;
     text: string;
     completed: boolean;
 }
@@ -13,42 +14,46 @@ export default function ModalTodoList() {
     const [modalTodoList, setModalTodoList] = useState<ModalTList[]>([
         {
             key: 1,
+            id: 1,
             text: '아르바이트',
             completed: false,
         },
         {
             key: 2,
+            id: 2,
             text: '청소',
             completed: false,
         }, {
             key: 3,
+            id: 3,
             text: '운동',
             completed: false,
         }
     ]);
 
     // modalTodoItem.tsx에서 전달 받은 인수 처리함수
-    const textUpdatedHandler = (newModalTodoItem: ModalTList): void => {
-        const newModalTodoList = modalTodoList.map((item) => {
-            if (item.key === newModalTodoItem.key) {
-                return newModalTodoItem;
+    const textUpdatedHandler = (newModalItem: ModalTList): void => {
+        const newItem = modalTodoList.map((item) => {
+            if (item.id === newModalItem.id) {
+                return newModalItem;
             } else {
                 return item;
             }
         })
-        setModalTodoList(newModalTodoList)
+        setModalTodoList(newItem);
     }
 
     return (
-        <>
+        <div>
             {modalTodoList.map((item) => (
                 <ModalTodoItem
                     key={item.key}
+                    id={item.id}
                     text={item.text}
                     completed={item.completed}
-                    onClickCompletedUpdate={textUpdatedHandler} />
+                    onClickCompleted={textUpdatedHandler} />
             ))
             }
-        </>
+        </div>
     )
 }
