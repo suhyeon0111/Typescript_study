66b38b3c1b4effb5edb577f8adcbc4c2f14131ee
suhyeon0111 <soo4833@naver.com>
2025-05-02 18:56:29 +0900

    feat: update api생성 (text 수정, completed 토글)

--- myWebPage/server/routes/todos.js
+++ myWebPage/server/routes/todos.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { getTodos, addTodo } = require("../controllers/todosController");
+const {
+  getTodos,
+  addTodo,
+  updateTodo,
+} = require("../controllers/todosController");
 
 // GET /todos?date=2025-04-26
 router.get("/", getTodos);
@@ -10,5 +14,8 @@ router.post("/", addTodo);
 
 // delete
 router.delete("/todos/:date/:id", deleteTodo);
+
+// update
+router.put("todos/:date/:id", updateTodo);
 
 module.exports = router;
